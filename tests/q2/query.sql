c4a72f89fad61000f1244cc435a843ea38d82f82
atshah <atshah@uwaterloo.ca>
2025-07-08 18:23:19 -0400

    Sample data and Sample query

--- tests/q2/query.sql
+++ tests/q2/query.sql
@@ -1,6 +1,8 @@
-WITH TeamAverage AS (
-SELECT AVG(FieldGoalAttempt) as avg
-FROM Player p, HasPlayer hp, Teams t WHERE p.PlayerID = hp.PlayerID and t.TeamID = hp.TeamID and t.TeamName = 'Toronto Raptors'
-)
-SELECT *
-FROM TeamAverage;
+SELECT g.GameID, stats.avg_points
+FROM Games g
+JOIN (
+    SELECT GameID AS gid, AVG(Points) AS avg_points
+    FROM PlayedIn
+    GROUP BY GameID
+) stats ON g.GameID = stats.gid
+WHERE g.GameDate >= '2024-10-25' AND g.GameDate <= '2024-10-27';
