3d179104399cc12ce7407d9d14b1af71f7c834c9
r2vasude <r2vasude@uwaterloo.ca>
2025-07-17 16:52:41 -0400

    updated division query, schema indexing

--- queries/answered_all_questions_as_user.sql
+++ queries/answered_all_questions_as_user.sql
@@ -1,21 +1,17 @@
--- Find all users who have answered every question that the current logged in user has answered
-SELECT DISTINCT u.Username
-FROM Users u
-WHERE NOT EXISTS (
-    -- Questions that the current user has answered
-    SELECT qa1.QuestionID
-    FROM QuizAttempts qa1
-    JOIN QuizAttemptItems qai1 ON qa1.AttemptID = qai1.AttemptID
-    JOIN Users u1 ON qa1.UserID = u1.UserID
-    WHERE u1.Username = '{{USERNAME}}'
-    
-    EXCEPT
-    
-    -- Questions that user u has answered
-    SELECT qa2.QuestionID
-    FROM QuizAttempts qa2
-    JOIN QuizAttemptItems qai2 ON qa2.AttemptID = qai2.AttemptID
-    WHERE qa2.UserID = u.UserID
-)
--- Exclude the current user from results
-AND u.Username != '{{USERNAME}}'
+SELECT u.Username
+FROM   Users u
+WHERE  u.Username <> '{{USERNAME}}'
+  AND NOT EXISTS (
+        SELECT *
+        FROM   QuizAttemptItems   qi_cur
+        JOIN   QuizAttempts       qa_cur ON qa_cur.AttemptID = qi_cur.AttemptID
+        JOIN   Users              cu     ON cu.UserID = qa_cur.UserID
+        WHERE  cu.Username = '{{USERNAME}}'
+          AND NOT EXISTS (
+                SELECT *
+                FROM   QuizAttemptItems   qi_other
+                JOIN   QuizAttempts       qa_other ON qa_other.AttemptID = qi_other.AttemptID
+                WHERE  qa_other.UserID = u.UserID
+                  AND  qi_other.QuestionID = qi_cur.QuestionID
+          )
+);
